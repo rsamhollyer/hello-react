b9c42600afc66fdde69f38d6c9c55dd018933da1
Sam Hollyer <rsamhollyer@gmail.com>
2021-01-20 13:03:32 -0600

    starting work with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import HelloReact from "./HelloReact";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Hello, React!</h1>
+				<p className="message">Wow! That's so coool!</p>
+				<HelloReact />
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/HelloReact.js
@@ -0,0 +1,9 @@
+//1. Import React
+import React from "react"; // similar to : Const React = require("react")
+
+//3. Write component
+function HelloReact() {
+	return <div className="message">Hello Custom Component</div>;
+}
+//2. Export component
+export default HelloReact; // similar to : module.exports = HelloReact
