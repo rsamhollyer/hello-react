46fd6d6c25f460cd5a9804e294cbf82b34bc1bcd
Sam Hollyer <rsamhollyer@gmail.com>
2021-01-21 12:01:02 -0600

    added conditional rendering example to gallery

--- src/PhotoCard.js
+++ src/PhotoCard.js
@@ -5,31 +5,47 @@ function PhotoCard(props) {
 
 	return (
 		<div className="photo-card">
-			<button
-				onClick={(e) => {
-					setCount(count + 1);
-				}}
-				className="like-button"
-			>
-				Like
-			</button>
-
-			<button
-				onClick={(e) => {
-					setCount(count > 0 ? count - 1 : 0);
-				}}
-				className="unlike-button"
-			>
-				UnLike
-			</button>
-
-			<p>Likes : {count}</p>
 			<img className="image" src={props.url} alt={props.description} />
 			<div className="card-body">
-				<footer className="card-footer"></footer>
 				<h5 className="photo-title">{props.title} </h5>
 				<p className="card-text">{props.description}</p>
 			</div>
+			{/*  */}
+			{/*  */}
+			{/*  */}
+			<footer className="card-footer">
+				<p className="card-likes">Likes : {count}</p>
+
+				<button
+					onClick={(e) => {
+						setCount(count + 1);
+					}}
+					className="like-button"
+				>
+					Like
+				</button>
+
+				{/* Disable the unlike button when the count of likes is equiv to 0 w/ Cond. Rend.*/}
+				{/*  */}
+				{/*  */}
+				{count > 0 ? (
+					<button
+						onClick={(e) => {
+							setCount(count > 0 ? count - 1 : 0);
+						}}
+						className="unlike-button"
+					>
+						UnLike
+					</button>
+				) : (
+					<button className="unlike-button" disabled>
+						UnLike
+					</button>
+				)}
+				{/*  */}
+				{/*  */}
+				{/*  */}
+			</footer>
 		</div>
 	);
 }
